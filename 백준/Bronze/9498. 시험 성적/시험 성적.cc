1f7bb5d7d0617dbc779842725ddc8e712bdadeb4
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-04-12 23:22:44 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    int a{};
+    
+    std::cin >> a;
+    
+    if (a>=90)
+    {
+        std::cout << "A" << "\n";
+    }
+    else if (a>=80)
+    {
+        std::cout << "B" << "\n";
+    }
+    else if (a>=70)
+    {
+        std::cout << "C" << "\n";
+    }
+    else if (a>=60)
+    {
+        std::cout << "D" << "\n";
+    }
+    else
+    {
+        std::cout << "F" << "\n";
+    }
+}
