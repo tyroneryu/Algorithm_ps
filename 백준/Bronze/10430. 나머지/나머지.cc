db2c604d8b839ebfdce34ddf2783c0597f4be6b7
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-04-12 22:26:46 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a{}, b{}, c{};
+    std::cin >> a >> b >> c;
+    std::cout << (a+b)%c << "\n";
+    std::cout << ((a%c)+(b%c))%c << "\n";
+    std::cout << (a*b)%c << "\n";
+    std::cout << ((a%c)*(b%c))%c << "\n";
+    
+    return 0;
+}
