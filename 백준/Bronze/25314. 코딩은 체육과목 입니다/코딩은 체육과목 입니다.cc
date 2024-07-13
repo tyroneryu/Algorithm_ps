8e390ecab2b00f5b1aa283955a37d923ed619019
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-07-13 17:26:16 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    
+    std::cin >> n;
+    
+    for(int i=0; i<n/4; i++)
+    {
+        printf("long ");
+    }
+    
+    printf("int\n");
+}
