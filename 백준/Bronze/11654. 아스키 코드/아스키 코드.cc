f1b3aa544005ccbae9dd27d1f46fc6e7a3c926fb
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-07-16 08:59:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <string>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    
+    cin >> x;
+    
+    cout << static_cast<int>(x) << "\n";
+    
+    
+    return 0;
+}
