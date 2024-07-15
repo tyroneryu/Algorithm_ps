b1e8accbda6927cb89d6b2cf68500e536f477d6e
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-07-15 17:32:39 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    
+    cin >> str;
+    
+    cout << str.length();
+    
+    return 0;
+}
