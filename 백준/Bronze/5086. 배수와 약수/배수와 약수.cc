b139614bfcb5ac18bfd953e75f745e4961ef3177
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-07-31 16:31:31 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    while (true) {
+        cin >> a >> b;
+        if (a == 0 && b == 0) break;
+
+        if (b % a == 0) {
+            cout << "factor" << endl;
+        } else if (a % b == 0) {
+            cout << "multiple" << endl;
+        } else {
+            cout << "neither" << endl;
+        }
+    }
+    return 0;
+}
