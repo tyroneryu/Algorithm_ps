714a7ac77e5a552ac9a912d1022721558a3a6838
tyroneryu <159678393+tyroneryu@users.noreply.github.com>
2024-08-01 16:22:21 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    long long n;
+    cin >> n;
+
+    cout << n * (n - 1) / 2 << "\n";
+    
+    cout << 2 << "\n";
+    
+    return 0;
+}
