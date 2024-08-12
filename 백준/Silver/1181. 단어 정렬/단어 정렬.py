513e4cc265fae0a0ac1e2c8181263577d3d21b04
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2024-08-12 11:24:09 +0900

    [Silver V] Title: 단어 정렬, Time: 828 ms, Memory: 36396 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+words = set()
+
+for _ in range(N):
+    words.add(input().strip())
+
+sorted_words = sorted(words, key=lambda x: (len(x), x))
+
+for word in sorted_words:
+    print(word)
