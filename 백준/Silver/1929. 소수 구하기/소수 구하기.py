d85725ef00cd168eb1bb9285033381b91b5aa965
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2024-08-13 13:45:44 +0900

    [Silver III] Title: 소수 구하기, Time: 156 ms, Memory: 38792 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,16 @@
+def sieve_of_eratosthenes(M, N):
+    is_prime = [True] * (N + 1)
+    is_prime[0] = is_prime[1] = False 
+    
+    for i in range(2, int(N**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, N + 1, i):
+                is_prime[j] = False
+
+    for i in range(M, N + 1):
+        if is_prime[i]:
+            print(i)
+
+M, N = map(int, input().split())
+
+sieve_of_eratosthenes(M, N)
