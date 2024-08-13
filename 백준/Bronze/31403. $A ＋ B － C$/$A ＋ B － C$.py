5c3073349cb79494351bab7f951b3f154cd3371d
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2024-08-13 14:12:15 +0900

    [Bronze IV] Title: $A + B - C$, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,9 @@
+a = input().strip()
+b = input().strip()
+c = input().strip()
+
+result_number = int(a) + int(b) - int(c)
+print(result_number)
+
+result_string = int(a + b) - int(c)
+print(result_string)
