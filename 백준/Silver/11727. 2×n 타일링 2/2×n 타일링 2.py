5221efddc633e260b1b9043fe787caa95db968ea
Taeyun Ryu <159678393+tyroneryu@users.noreply.github.com>
2024-08-16 11:58:25 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+def tile_count(n):
+    dp = [0] * (n + 1)
+    
+    dp[1] = 1
+    if n > 1:
+        dp[2] = 3
+    
+    for i in range(3, n + 1):
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007
+    
+    return dp[n]
+
+n = int(input().strip())
+
+print(tile_count(n))
